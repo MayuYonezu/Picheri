1387910ac3ede0c2d849136ab92e962f5fb9ab5a
MayuYonezu <s2122054@stu.musashino-u.ac.jp>
2023-09-09 00:13:37 +0900

    ボタンが押された時のアクションを書きました

--- Picheri/Presenter/LoginPresenter.swift
+++ Picheri/Presenter/LoginPresenter.swift
@@ -1,8 +1,27 @@
-//
-//  LoginPresenter.swift
-//  Picheri
-//
-//  Created by Mayu Yonezu on 2023/09/08.
-//
+protocol LoginPresenter {
+    func updateTitleAndButton(isLogin: Bool)
+}
 
-import Foundation
+protocol LoginView: AnyObject {
+    func updateTitleAndButton(title: String)
+    func setButtonTitle(title: String)
+}
+
+class LoginPresenterImpl: LoginPresenter {
+    
+    weak var view: LoginView?
+
+    init(view: LoginView) {
+        self.view = view
+    }
+
+    func updateTitleAndButton(isLogin: Bool) {
+        if isLogin {
+            view?.updateTitleAndButton(title: "Login")
+            view?.setButtonTitle(title: "signup")
+        } else {
+            view?.updateTitleAndButton(title: "Sign up")
+            view?.setButtonTitle(title: "login")
+        }
+    }
+}
